0eb038e772129d4a1b75e7b01d7811a20ca419ef
DanielGorin <daniel.l.gorin@gmail.com>
2024-11-22 18:50:29 +0200

    Improved the Evaluation feature by editing the text outputs to be more readable.

--- Models/Claims.cs
+++ Models/Claims.cs
@@ -7,23 +7,23 @@ namespace PROG_2B_POE_Final_Part_ST10438307_Daniel_Gorin.Models
     public class Claims
     {
         [Key]
-        public int ClaimId { get; set; }
+        public int ClaimId { get; set; }//Primary Key
         [Required]
         [Column(TypeName = "nvarchar(max)")]
-        public string ClaimantName { get; set; }
+        public string ClaimantName { get; set; }//First and Last Name of Claimant
         [Required]
-        public decimal HourlyRate { get; set; }
+        public decimal HourlyRate { get; set; }//Amount payed per hour in Rands
         [Required]
-        public int HoursWorked { get; set; }
+        public int HoursWorked { get; set; }//number fo hours worked for the claim
         [Required]
         [Column(TypeName = "nvarchar(max)")]
-        public string ClaimantComments { get; set; }
+        public string ClaimantComments { get; set; }//any additional comments the claimant may have
         [Required]
-        public DateTime DateLogged { get; set; }
+        public DateTime DateLogged { get; set; }//Date the claimant submited the claim
         [Column(TypeName = "nvarchar(max)")]
         public string UploadedFiles { get; set; } = "file";
         [Required]
         [Column(TypeName = "nvarchar(max)")]
-        public string Status { get; set; }
+        public string Status { get; set; }//Whther the claim is pending, accepted or denied
     }
 }
